6600e0036cbc41c1a5dd97ee4c269d59a0bd7dc6
Mayank Kathuria <mayank4kathuria@gmail.com>
2025-01-22 23:28:24 +0530

    WIP adds collection and best selling section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -274,20 +274,10 @@ export default function Home() {
             </div>
           </div>
 
-          {/* <div className="flex justify-center overflow-clip translate-y-10 -rotatee-12 scale-1 pb-10 opacity-100 " >
-            <Image
-              className="w-full"
-              height={980}
-              width={1080}
-              quality={100}
-              src={'/men-s-shoes-clothing-accessories.jpg'}
-              alt={NEW_ARRIVALS[0]?.name}
-            />
-          </div> */}
         </section>
 
-        <div className="py-8" >
-          <div className="flex" >
+        <section className="py-16" >
+          <div className="flex ml-6 mb-10" >
             <div className="min-w-44 flex self-center px-6 py-2 bg-indigo-950 border-4 border-indigo-900 rounded-full hover:bg-indigo-900" >
               <p className="text-sm text-white uppercase" >New Arrivals</p>
             </div>
@@ -299,11 +289,11 @@ export default function Home() {
           {/* Other shoes with listing */}
           {/* <p className="text-md font-bold text-gray-400 uppercase mb-4" >Popular</p> */}
 
-          <div className="w-full flex flex-wrap mt-8 py-8" >
+          <div className="w-full flex flex-wrap pb-16 border-0 border-b border-slate-400" >
 
-            {SHOES_LISTINGS.map((shoe, index) =>
-              <div key={`${index}-${shoe?.name}`} className="w-80 flex flex-col p-4 m-6 shadow-slate-200 rounded-xl shadow-sm hover:shadow-lg cursor-pointer" >
-                <div className="w-full flex justify-center bg-neutral-100" >
+            {SHOES_LISTINGS.slice(0, 11).map((shoe, index) =>
+              <div key={`${index}-${shoe?.name}`} className="w-80 flex flex-col mx-6 mb-16 shadow-slate-200 shadow-sm hover:shadow-lg cursor-pointer" >
+                <div className="w-full flex justify-center bg-neutral-100 rounded-t-lg " >
                   <Image
                     height={250}
                     width={250}
@@ -311,7 +301,7 @@ export default function Home() {
                     alt={shoe?.name || ''}
                   />
                 </div>
-                <div className="flex-1 flex flex-col mt-2" >
+                <div className="flex-1 flex flex-col p-4 mte-2" >
                   <p className="text-md font-bold text-black">
                     {shoe?.name}
                   </p>
@@ -329,12 +319,88 @@ export default function Home() {
               </div>)}
 
           </div>
-        </div>
+        </section>
+
+        <section className="py-36 mb-16 flex justify-center items-center" >
+          {/* Picture of 2 shoes */}
+          <div className="h-[600px] w-[400px] relative bg-yellow-300 mr-8 -translate-y-20" >
+            <div className="absolute bottom-8 p-8">
+              <p className="uppercase text-2xl mb-4" >Famous Muiches</p>
+              <div className="flex container" >
+                <p className="p-2 border border-white rounded-lg cursor-pointer hover:bg-white hover:text-black" >
+                  SHOP NOW
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="h-[600px] w-[400px] relative bg-sky-300 mr-8 translate-y-20" >
+            <div className="absolute bottom-8 p-8">
+              <p className="uppercase text-2xl mb-4" >Special Collection</p>
+              <div className="flex container" >
+                <p className="p-2 border border-white rounded-lg cursor-pointer hover:bg-white hover:text-black" >
+                  SHOP NOW
+                </p>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <section className="mb-8 p-4" >
+          {/* Best Selling collection */}
+          <div className="flex justify-between items-center text-black ml-2" >
+            <p className="uppercase text-4xl text-white font-light drop-shadow-[-2.5px_0.5px_1.0px_rgba(0,0,0,1)]" >Best <span className="text-black drop-shadow-none">Sell</span>ing</p>
+            <div className="flex items-center">
+              <p className="py-2 px-4 mr-2 cursor-pointer hover:bg-neutral-400">PREV</p>
+              <p className="py-2 px-4 mr-2 cursor-pointer hover:bg-neutral-400">NEXT</p>
+            </div>
+          </div>
+
+          <div className="w-full flex my-8 py-8 overflow-x-auto" >
+
+            {SHOES_LISTINGS.slice(0, 11).map((shoe, index) =>
+              <div key={`${index}-${shoe?.name}`} className="min-w-80 flex flex-col mr-16 shadow-slate-200 shadow-sm hover:shadow-lg cursor-pointer" >
+                <div className="w-full flex justify-center bg-neutral-100 rounded-t-lg " >
+                  <Image
+                    height={250}
+                    width={250}
+                    src={shoe?.imageLink || ''}
+                    alt={shoe?.name || ''}
+                  />
+                </div>
+                <div className="flex-1 flex flex-col p-4 mte-2" >
+                  <p className="text-md font-bold text-black">
+                    {shoe?.name}
+                  </p>
+                  <p className="flex flex-col my-2">
+                    <span className="text-sm text-black">
+                      {shoe?.price}
+                    </span>
+                  </p>
+                  <p className="flex flex-col">
+                    <span className="text-sm text-black">
+                      Stars
+                    </span>
+                  </p>
+                </div>
+              </div>)}
+
+          </div>
+
+        </section>
+
+        <section className="py-8" >
+          {/* Picture of categories available */}
+          <div className="text-black bg-red-200" >Will be available soon</div>
+        </section>
+
+        <section>
+          {/* Picture of service promise */}
+        </section>
 
       </div>
 
       {/* Footer */}
-      <div className="w-full p-4 bg-black flex justify-center items-center`">
+      <div className="w-full p-4 bg-stone-800 flex justify-center items-center`">
         <p>Made By Mayank Kathuria</p>
       </div>
     </main>
